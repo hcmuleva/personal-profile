9aee64e56c756c7de4c9155ebef6d86ee6cc7806
Arpita14378 <arpitahk@gmail.com>
2020-07-20 21:31:41 +0530

    Backup purpose

---
+++ Lms_Arpita/UserBook/User Login.py
@@ -0,0 +1,25 @@
+from Lms_Arpita.DatabaseConnectivity import MongoConnection
+p = MongoConnection.path()
+collection = p.db["UserLogBook"]
+
+
+class UserLogin:
+    def create_user(self,user_name,user_id,password,age,gender,location):
+        user_details = {
+            "Name": user_name,
+            "Email_id": user_id,
+            "Password": password,
+            "Age": age,
+            "Gender": gender,
+            "Location": location
+        }
+        return collection.insert_one(user_details)
+    def login(self,user_id,password):
+        access = collection.find_one({"Email_id": user_id, "Password": password})
+        if access is None:
+            print("access denied plz check ur id and password")
+        else:
+            print("welcome now u have access to ur data")
+
+        pass
+
