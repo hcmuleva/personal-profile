6ecee64719c653614c5df49ffbb5842275c77cbe
adityanmuleva <aadityamuleva@gmail.com>
2020-09-08 12:46:02 +0530

    Testing

---
+++ lms_aaditya/Test/CreateTest.py
@@ -0,0 +1,22 @@
+import json
+# from MongoConnect import ConnectModule
+# a = ConnectModule.connect()
+# collection = a.db["Contacts"]
+
+from pymongo import MongoClient
+client = MongoClient('mongodb://localhost:27017/')
+db = client['lms_db']
+collection = db["TestContacts"]
+
+uid = "temp"
+
+
+class TestCreateContact:
+    def test_create(self):
+        global uid
+        my_data = {
+            "name": "harish",
+            "last_name": "Muleva"
+        }
+        uid = collection.insert_one(my_data).inserted_id
+        assert uid is not None
